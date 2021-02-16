086550a7ff047dc55ba333c54f0541d78d104a1a
Aslan <aslanstamaliev@gmail.com>
2021-02-16 13:10:38 +0600

    добавил функционал поиска и фильтр по региону

--- script.js
+++ script.js
@@ -3,40 +3,28 @@ const output = document.querySelector('.output');
 const body = document.querySelector('body');
 const byRegion = document.querySelector('#byRegion');
 
+let allCountries;
 const result = [];
-const allCountries = [];
+const html = [];
 
-function getCountriesInfo() {
-	fetch('https://restcountries.eu/rest/v2/all').then(response => {
+const getCountriesInfo = async () => {
+	allCountries = await fetch('https://restcountries.eu/rest/v2/all?fields=name;nativeName;flag;papulation;region;capital')
+	.then(response => {
 		if (!response.ok) {
 			throw new Error(`HTTP error! status: ${response.status}`);
 		} else {
 			return response.json();
 		}
-	}).then(listCount => {
-		listCount.forEach((item) => {
-			allCountries.push(item);
-		});
-		tagWrapper(listCount);
 	}).catch(e => {
 		console.log(`There has been a problem with your fetch operation: ${e.message}`);
-	})
-}
-
-byRegion.addEventListener('change', (event) => {
-	let curRegion = event.target.value;
-	let regCount = [];
-	allCountries.forEach((item) => {
-		if (curRegion === item.region) {
-			regCount.push(item);
-		}
 	});
-	tagWrapper(regCount);
-});
+	wrapperTag(allCountries);
+};
 
-function tagWrapper(list) {
-	result.length = 0;
-	list.forEach((item) => {
+function wrapperTag(contentToWrap) {
+	output.innerHTML = "";
+	html.length = 0;
+	contentToWrap.forEach((item) => {
 		const country = `<div class="col">
 											<img class="flag" src="${item.flag}">
 											<div class="outPrc">
@@ -48,10 +36,39 @@ function tagWrapper(list) {
 												</div>
 											</div>
 										</div>`;
-		result.push(country);
+		html.push(country);
 	});
-	output.innerHTML = "";
-	output.innerHTML = result.join('');
+	output.innerHTML = html.join('');
 };
 
+byRegion.addEventListener('change', (event) => {
+	result.length = 0;
+	let curRegion = event.target.value;
+	allCountries.forEach((item) => {
+		if (curRegion === item.region) {
+			result.push(item);
+		}
+	});
+	wrapperTag(result);
+});
+
+searchBy.addEventListener('input', (e) => {
+	result.length = 0;
+	output.innerHTML = '';
+	let i = e.target.value.toLowerCase();
+	allCountries.filter(country => {
+		if(country.name.toLowerCase().includes(i)) {
+			result.push(country);
+		} else {
+			console.log('No matches');
+		};
+	});
+	wrapperTag(result);
+});
+
+
+
 getCountriesInfo();
+
+// https://codepen.io/FlorinPop17/pen/qzNxGa/?editors=0010
+// https://www.florin-pop.com/blog/2019/06/vanilla-javascript-instant-search/
